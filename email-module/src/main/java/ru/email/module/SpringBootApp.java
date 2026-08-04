53fee9b6ce533ecb4be8788ee7c4f454bb0f43f4
Gleichmut <alena20010421@gmail.com>
2026-08-04 22:03:43 +0300

    Add new module

--- email-module/src/main/java/ru/email/module/SpringBootApp.java
+++ email-module/src/main/java/ru/email/module/SpringBootApp.java
@@ -1,5 +1,11 @@
 package ru.email.module;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringBootApp {
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
 }
