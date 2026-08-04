6b91ad7ff6585f851bbe447bf02dc7d05635d835
Gleichmut <alena20010421@gmail.com>
2026-08-04 17:58:40 +0300

    Add GET, PUT, DELETE requests for ProductController

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,16 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.CategoryCreateRequest;
+import com.example.demo.dto.CategoryResponse;
 import com.example.demo.dto.ProductCreateRequest;
 import com.example.demo.dto.ProductResponse;
+import com.example.demo.entity.Category;
+import com.example.demo.entity.Product;
 import com.example.demo.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/product")
@@ -54,5 +57,23 @@ public class ProductController {
     @ApiResponse(responseCode = "200", description = "Выброшено исключение")
     public ProductResponse throwException2() {
         throw new RuntimeException("Ошибочка2");
+    }
+
+    @GetMapping("/getAllProducts")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PutMapping("/updateProduct/{id}")
+    public ProductResponse updateCategory(
+            @PathVariable Long id,
+            @RequestBody ProductCreateRequest request) {
+        return productService.updateProduct(id, request);
+    }
+
+    @DeleteMapping("/deleteProduct/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "Продукт с id " + id + " успешно удален";
     }
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -4,9 +4,12 @@ import com.example.demo.dto.ProductCreateRequest;
 import com.example.demo.dto.ProductResponse;
 import com.example.demo.entity.Product;
 import com.example.demo.exceptions.ProductExists;
+import com.example.demo.exceptions.ProductNotFound;
 import com.example.demo.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -29,7 +32,31 @@ public class ProductService {
         }
         return new ProductResponse(product.getId(), product.getTitle());
     }
+
+    public List<Product> getAllProducts() {
+        if (productRepository.findAll().isEmpty()) {
+            throw new ProductNotFound("Список  пуст или категории не найдены.");
+        }
+        return productRepository.findAll();
+    }
+
+    public ProductResponse updateProduct(Long id, ProductCreateRequest request) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductExists("Продукт: " + request.getTitle() + " не найден."));
+        product.setTitle(request.getTitle());
+        Product updatesProduct = productRepository.save(product);
+        return new ProductResponse(updatesProduct.getId(), updatesProduct.getTitle());
+    }
+
+    public void deleteProduct(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new ProductExists("Продукт с id = " + id + " не найден.");
+        }
+        productRepository.deleteById(id);
+    }
 }
+
+
 
 // сделать для продуктов и категорий базовые rest запросы get put delete и добавить исключения свои
 // kafka, reddis, микро
