37885d0408f240072f6ab13362acb95d5fc04794
Gleichmut <alena20010421@gmail.com>
2026-08-21 17:03:16 +0300

    Add connection between ProductResponse and Product

--- src/main/java/com/example/demo/dto/ProductResponse.java
+++ src/main/java/com/example/demo/dto/ProductResponse.java
@@ -1,18 +1,26 @@
 package com.example.demo.dto;
 
+import com.example.demo.entity.Product;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class ProductResponse {
     private Long id;
     private String title;
+    private BigDecimal price;
+    private Long categoryId;
 
-    public ProductResponse(Long id, String title) {
-        this.id = id;
-        this.title = title;
+    public ProductResponse(Product product) {
+        this.id = product.getId();
+        this.title = product.getTitle();
+        this.price = product.getPrice();
+        this.categoryId = product.getCategory() != null ?
+                product.getCategory().getId() : null;
     }
-
-    public ProductResponse() {
-    }
-
 }
