ea47545d356d925bc3ad51aae11f11fc2baa02fe
Gleichmut <alena20010421@gmail.com>
2026-08-21 16:53:34 +0300

    Add connection between CategoryResponse and Category

--- src/main/java/com/example/demo/dto/CategoryResponse.java
+++ src/main/java/com/example/demo/dto/CategoryResponse.java
@@ -1,19 +1,20 @@
 package com.example.demo.dto;
 
+import com.example.demo.entity.Category;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class CategoryResponse {
     private Long id;
     private String name;
 
-    public CategoryResponse(Long id, String name) {
-        this.id = id;
-        this.name = name;
+    public CategoryResponse(Category category) {
+        this.id = category.getId();
+        this.name = category.getName();
     }
-
-    public CategoryResponse() {
-    }
-
 }
 
