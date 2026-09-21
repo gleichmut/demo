2686b57359c924adbad935c0fb4c849b5f51f8f3
Gleichmut <alena20010421@gmail.com>
2026-09-21 16:28:11 +0300

    Add @Getter and @Setter, comment constructor

--- src/main/java/com/example/demo/dto/CategoryResponse.java
+++ src/main/java/com/example/demo/dto/CategoryResponse.java
@@ -1,20 +1,21 @@
 package com.example.demo.dto;
 
-import com.example.demo.entity.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class CategoryResponse {
     private Long id;
     private String name;
 
-    public CategoryResponse(Category category) {
-        this.id = category.getId();
-        this.name = category.getName();
-    }
+//    public CategoryResponse(Category category) {
+//        this.id = category.getId();
+//        this.name = category.getName();
+//    }
 }
 
